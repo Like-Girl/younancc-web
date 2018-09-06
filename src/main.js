45c7072784815e184fb38a732e1897e53eb83951
ke.chen <likegirl.cn@gmail.com>
2018-09-06 14:35:19 +0800

    引用bootstarp等插件

--- src/main.js
+++ src/main.js
@@ -5,14 +5,23 @@ import App from './App'
 import router from './router'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/plugins/fontawesome/css/font-awesome.min.css'
+import './assets/plugins/themify-icons/themify-icons.min.css'
+import './assets/plugins/animate.css/animate.min.css'
+import './assets/plugins/perfect-scrollbar/perfect-scrollbar.min.css'
+import './assets/plugins/switchery/switchery.min.css'
+import './assets/css/styles.css'
+import './assets/css/plugins.css'
+import './assets/css/themes/theme-1.css'
 
 import 'bootstrap/dist/js/bootstrap.min.js'
-import './assets/plugins/modernizr/modernizr.js'
+// import './assets/plugins/modernizr/modernizr.js'
 import './assets/plugins/jquery-cookie/jquery.cookie.js'
 import './assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js'
 import './assets/plugins/switchery/switchery.min.js'
 import './assets/plugins/Chart.js/Chart.min.js'
 import './assets/plugins/jquery.sparkline/jquery.sparkline.min.js'
+import './assets/plugins/jquery-validation/jquery.validate.min'
 
 Vue.config.productionTip = false
 
